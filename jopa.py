ea4f2308a1a12f54cd7eac0d05d5e8f093a1489b
Liza Antonova <91476351+lizzka-kisska@users.noreply.github.com>
2023-06-01 23:24:31 +0500

    удаление ненужных файлов

--- jopa.py
+++
@@ -1,9 +0,0 @@
-with open('configuration/config.txt') as f:
-    data = f.read().split('\n')
-    mails = data[0].split(', ')
-    TO = ''
-    for mail in mails:
-        TO += mail + ','
-    print(TO)
-
-
